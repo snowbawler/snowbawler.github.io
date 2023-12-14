87f9f2e576b31446c64a885a00f571af7f90375f
snowbawler <eglaf0@gmail.com>
2023-12-14 16:46:56 -0600

    scroll pointer events

---
+++ src/components/helper/ScrollPointerEvents.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  function handleScroll() {
+    // Calculate the scroll percentage
+    const scrollPercentage = (window.scrollY + window.innerHeight) / document.documentElement.scrollHeight * 100;
+
+    // Get the element with the class 'pin-spacer'
+    const pinSpacer = document.querySelector('.pin-spacer');
+
+    // Check if the scroll percentage is at the bottom (adjust the threshold as needed)
+    if (scrollPercentage === 100) {
+      pinSpacer.style.pointerEvents = 'none';
+    } else {
+      pinSpacer.style.pointerEvents = 'auto';
+    }
+  }
+
+  // Attach the scroll event listener
+  window.addEventListener('scroll', handleScroll);
+});
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Projects from "./components/Projects"
 import Scroll from "./components/Scroll.jsx"
+import Home from './pages/Home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Projects />
-    <Scroll />
+    <Home/>
   </React.StrictMode>,
 )

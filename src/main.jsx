b19a3d51648d768ec00ee7e2c3f258927648a058
snowbawler <eglaf0@gmail.com>
2023-12-16 01:28:51 -0600

    s

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Projects from "./components/Projects"
-import Scroll from "./components/Scroll.jsx"
-import Home from './pages/Home.jsx'
 import App from './Router.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(

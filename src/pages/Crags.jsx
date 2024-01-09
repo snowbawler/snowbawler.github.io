7029d1167bd9d9e5e118db28b2cbdd8bfe94ef90
snowbawler <eglaf0@gmail.com>
2024-01-09 13:02:55 -0600

    s

--- src/pages/Crags.jsx
+++ src/pages/Crags.jsx
@@ -6,8 +6,17 @@ import CragCase from "../assets/CragCase.svg";
 const Crags = () => {
     return (
         <>
-            <img src={CragCase} style={{ position: 'relative', maxWidth:'100vw', width: '100vw', height: 'auto', objectFit: 'contain' }} />
-            <Link preventScrollReset={true} to="/">Back</Link>
+            <img src={CragCase} style={{ position: 'absolute', maxWidth:'100vw', width: '100vw', height: 'auto', objectFit: 'contain' }} />
+            <Link preventScrollReset={true} to="/" style={{                    position: "absolute",
+                    top: "20px", // Adjust the top position according to your design
+                    left: "20px", // Adjust the left position according to your design
+                    padding: "10px 15px",
+                    background: "#F8971F", // Set the background color
+                    color: "#fff", // Set the text color
+                    textDecoration: "none",
+                    borderRadius: "5px", // Add rounded corners
+                    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", // Add a subtle shadow
+                    fontSize: "16px",}}>Back</Link>
         </>
     );
 };

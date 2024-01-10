285f71a4e7f8e8de332ccda26d9fe98c5f46ec31
snowbawler <eglaf0@gmail.com>
2024-01-10 16:12:08 -0600

    s

--- src/pages/Muck.jsx
+++ src/pages/Muck.jsx
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom"
 import transition from "../components/helper/transition";
 import ReactPlayer from 'react-player'
 import MuckTrailer from '../assets/MuckTrailer.mp4'
+import './muck.css'
 
 const Muck = () => {
     return(
         <>
-            <ReactPlayer url={MuckTrailer}/>
+            <div className="title">Muck.</div>
+            <div className="video-container">    
+                <ReactPlayer url={MuckTrailer}
+                    controls={true}
+                    volume={.15}
+                    className='video'
+                />
+            </div>
             <Link preventScrollReset={true} to="/" style={{position: "absolute", top: "20px", left: "20px", padding: "10px 15px", background: "#F8971F", color: "#fff",  textDecoration: "none", borderRadius: "5px",  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", fontSize: "16px",}}>Back</Link>
         </>
     )

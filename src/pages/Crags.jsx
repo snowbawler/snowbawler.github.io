f4ac8c0d4290c807acfcc56597e84a5e52c0f259
snowbawler <eglaf0@gmail.com>
2024-01-10 17:27:12 -0600

    s

--- src/pages/Crags.jsx
+++ src/pages/Crags.jsx
@@ -7,7 +7,7 @@ const Crags = () => {
     return (
         <>
             <img src={CragCase} style={{ position: 'absolute', maxWidth:'100vw', width: '100vw', height: 'auto', objectFit: 'contain' }} />
-            <Link preventScrollReset={true} to="/" style={{                    position: "absolute",
+            <Link to="/" style={{                    position: "absolute",
                     top: "20px", // Adjust the top position according to your design
                     left: "20px", // Adjust the left position according to your design
                     padding: "10px 15px",
--- src/pages/Muck.jsx
+++ src/pages/Muck.jsx
@@ -1,13 +1,19 @@
 import React from "react"
+import { useEffect } from 'react';
 import { Link } from "react-router-dom"
 import transition from "../components/helper/transition";
 import ReactPlayer from 'react-player'
 import MuckTrailer from '../assets/MuckTrailer.mp4'
-import './muck.css'
+import './pages.css'
 
 const Muck = () => {
+
+    useEffect(() => {
+        window.scrollTo(0, 0); // Scrolls to the top when the component mounts
+    }, []);
+
     return(
-        <>
+        <div className="muckBody">
             <div className="title">Muck.</div>
             <div className="video-container">    
                 <ReactPlayer url={MuckTrailer}
@@ -16,8 +22,9 @@ const Muck = () => {
                     className='video'
                 />
             </div>
-            <Link preventScrollReset={true} to="/" style={{position: "absolute", top: "20px", left: "20px", padding: "10px 15px", background: "#F8971F", color: "#fff",  textDecoration: "none", borderRadius: "5px",  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", fontSize: "16px",}}>Back</Link>
-        </>
+            <Link to="/" style={{position: "absolute", top: "20px", left: "20px", padding: "10px 15px", background: "#F8971F", color: "#fff",  textDecoration: "none", borderRadius: "5px",  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", fontSize: "16px",}}>Back</Link>
+            
+        </div>
     )
 }
 

1fb1cc6954b59c07c667061849dc6050070af52d
snowbawler <eglaf0@gmail.com>
2024-01-11 21:09:58 -0600

    update muck

--- src/pages/Muck.jsx
+++ src/pages/Muck.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom"
 import transition from "../components/helper/transition";
 import ReactPlayer from 'react-player'
 import MuckTrailer from '../assets/MuckTrailer.mp4'
+import MuckPlan from '../assets/MuckPlan.svg'
+import GPB from '../assets/GPB.png'
+import pitch from '../assets/pitch.png'
+import pitchpdf from '../assets/pitch.pdf'
 import './pages.css'
 
 const Muck = () => {
@@ -23,7 +27,15 @@ const Muck = () => {
                 />
             </div>
             <Link to="/" style={{position: "absolute", top: "20px", left: "20px", padding: "10px 15px", background: "#F8971F", color: "#fff",  textDecoration: "none", borderRadius: "5px",  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", fontSize: "16px",}}>Back</Link>
-            
+            <div className="heading">Project Roadmap</div>
+            <img className="muckPlan" src={MuckPlan}></img>
+            <div className="heading">Ideate</div>
+            <div className="muckGP-container">
+                <a href={pitchpdf}  target="_blank">
+                    <img className="muckGP" src={pitch} alt="" />
+                </a>
+                <img className="muckGP" src={GPB}></img>
+            </div>
         </div>
     )
 }

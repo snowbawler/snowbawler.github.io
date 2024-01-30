aa5324e863a2356978b0962d815506d12b9fe741
snowbawler <eglaf0@gmail.com>
2024-01-30 01:54:11 -0600

    s

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -34,9 +34,7 @@ export default function Projects() {
         <a href="https://github.com/snowbawler" target='_blank'>
           <img className='link' src={gitHubIcon} alt="GitHub" />
         </a>
-        <a href={resumePDF} target='_blank'>
-          <img className='link' src={resume} alt="Resume" />
-        </a>
+
         <a href="https://linkedin.com/in/donne-su" target='_blank'>
           <img className='link' src={linkedin} alt="LinkedIn" />
         </a>

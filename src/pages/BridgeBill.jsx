d34e868cba774b941a70d6ba4b2cc0dca2cf9444
snowbawler <eglaf0@gmail.com>
2024-05-08 22:59:58 -0500

    back button

--- src/pages/BridgeBill.jsx
+++ src/pages/BridgeBill.jsx
@@ -3,6 +3,7 @@ import { NotionRenderer } from 'react-notion';
 import "react-notion/src/styles.css";
 import "./pages.css"
 import banner from "../assets/bridgebillbanner.png";
+import { Link } from "react-router-dom"
 
 const NOTION_PAGE_ID = 'eae685212778405d9b264bb36ed92801';
 
@@ -22,6 +23,8 @@ export default function NotionPage() {
 
   return (
      <> 
+      <Link to="/" style={{position: "absolute", top: "20px", left: "20px", padding: "10px 15px", background: "#8BB7BA", color: "#fff",  textDecoration: "none", borderRadius: "5px",  boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)", fontSize: "16px",}}>Back</Link>
+
       <img src={banner} alt="BridgeBill Banner" style={{maxWidth: '100vw', width: '100vw'}}></img>
       <h1 className='notion-title notion parent'>BridgeBill</h1>
       <div className='parent' style={{ maxWidth: 768 }}>
